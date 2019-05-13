599f11bc019a83997fbd87ad12ebafacb78222fd
laschuet <laschuet@users.noreply.github.com>
2019-05-13 14:03:15 +0200

    Remove strings from data samples

--- test/clustering.jl
+++ test/clustering.jl
@@ -2,17 +2,17 @@ using VAIML
 using Test
 
 @testset "Clustering" begin
-    c = PartitionalClustering([1 "a"; 0 "a"], [0 1; 1 0], [0 1.0; 1.0 0],
-            [1, 1], [0.5, "a"])
-    @test data(c) == [1 "a"; 0 "a"]
+    c = PartitionalClustering([1 1; 0 1], [0 1; 1 0], [0 1.0; 1.0 0], [1, 1],
+            [0.5, 1])
+    @test data(c) == [1 1; 0 1]
     @test constraints(c) == [0 1; 1 0]
     @test weights(c) == [0 1.0; 1.0 0]
     @test assignments(c) == [1, 1]
     @test centers(c) == [0.5, "a"]
     @test θ(c) == ([0.5, "a"])
 
-    c = HierarchicalClustering([1 "a"; 0 "a"], [0 1; 1 0], [0 1.0; 1.0 0])
-    @test data(c) == [1 "a"; 0 "a"]
+    c = HierarchicalClustering([1 2; 0 2], [0 1; 1 0], [0 1.0; 1.0 0])
+    @test data(c) == [1 2; 0 2]
     @test constraints(c) == [0 1; 1 0]
     @test weights(c) == [0 1.0; 1.0 0]
 end
