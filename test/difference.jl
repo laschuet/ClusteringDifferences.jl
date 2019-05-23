86ef193f0c383b1a7fe07e06ba7edabbef511cff
laschuet <laschuet@users.noreply.github.com>
2019-05-23 08:01:51 +0200

    Overhaul tests

--- test/difference.jl
+++ test/difference.jl
@@ -8,15 +8,23 @@
     end
 
     @testset "operator" begin
+        c = b - a
+        @test (c.X == [1 1; 2 1] && c.C == [0 -2; -2 0] && c.W == [0 0; 0 0]
+                && c.Y == [-1, 0] && c.M == [1.5 1])
     end
 
     @testset "forward difference" begin
         @test forward([a, b], 2) == nothing
-        @test forward([a, b], 1) == b - a
+        c = forward([a, b], 1)
+        @test (c.X == [1 1; 2 1] && c.C == [0 -2; -2 0] && c.W == [0 0; 0 0]
+                && c.Y == [-1, 0] && c.M == [1.5 1])
     end
 
     @testset "backward difference" begin
-        @test backward([a, b], 1) == a
-        @test backward([a, b], 2) == b - a
+        c = backward([a, b], 1)
+        @test c.X == a.X && c.C == a.C && c.W == a.W && c.Y == a.Y && c.M == a.M
+        c = backward([a, b], 2)
+        @test (c.X == [1 1; 2 1] && c.C == [0 -2; -2 0] && c.W == [0 0; 0 0]
+                && c.Y == [-1, 0] && c.M == [1.5 1])
     end
 end
