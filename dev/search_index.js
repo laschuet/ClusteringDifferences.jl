0a4b5d4574af130832a4a9e824e798e221fc07f9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-23 06:09:34 +0000

    build based on 86ef193

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#VAIML.jl-1",
+    "page": "Home",
+    "title": "VAIML.jl",
+    "category": "section",
+    "text": ""
+},
+
+]}
