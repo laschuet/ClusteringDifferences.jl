89ec8791f4c743c3fabc608719ce1537ba7a93ee
laschuet <laschuet@users.noreply.github.com>
2019-06-12 07:41:58 +0200

    Make tests more meaningful

--- test/clustering.jl
+++ test/clustering.jl
@@ -1,56 +1,59 @@
 @testset "clustering" begin
     @testset "partitional clustering" begin
-        pc = PartitionalClustering([1 1; 0 1], [-1 1; 1 0], [0 1.0; 1.0 0],
-                [1.0 0.0; 0.3 0.7], [0.5 1])
+        pc = PartitionalClustering([0 1; 1 0; 1 1], [0 0 0; 0 0 0; 0 0 0],
+                [0 0 0; 0 0 0; 0 0 0], [1.0 0.0; 0.0 1.0; 0.5 0.5], [0 1; 1 0])
 
         @testset "constructors" begin
-            @test(pc.X == [1 1; 0 1]
-                && pc.C == [-1 1; 1 0]
-                && pc.W == [0 1.0; 1.0 0]
-                && pc.Y == [1.0 0.0; 0.3 0.7]
-                && pc.M == [0.5 1])
+            @test(pc.X == [0 1; 1 0; 1 1]
+                && pc.C == [0 0 0; 0 0 0; 0 0 0]
+                && pc.W == [0 0 0; 0 0 0; 0 0 0]
+                && pc.Y == [1.0 0.0; 0.0 1.0; 0.5 0.5]
+                && pc.M == [0 1; 1 0])
         end
 
         @testset "==" begin
             @test pc == pc
-            pc2 = PartitionalClustering([1 1; 0 1], [-1 1; 1 0],
-                    [0 1.0; 1.0 0], [1.0 0.0; 0.3 0.7], [0.5 1])
+            pc2 = PartitionalClustering([0 1; 1 0; 1 1], [0 0 0; 0 0 0; 0 0 0],
+                    [0 0 0; 0 0 0; 0 0 0], [1.0 0.0; 0.0 1.0; 0.5 0.5],
+                    [0 1; 1 0])
             @test pc == pc2 && pc2 == pc
-            pc3 = PartitionalClustering([1 1; 0 1], [-1 1; 1 0],
-                    [0 1.0; 1.0 0], [1.0 0.0; 0.3 0.7], [0.5 1])
+            pc3 = PartitionalClustering([0 1; 1 0; 1 1], [0 0 0; 0 0 0; 0 0 0],
+                    [0 0 0; 0 0 0; 0 0 0], [1.0 0.0; 0.0 1.0; 0.5 0.5],
+                    [0 1; 1 0])
             @test pc == pc2 && pc2 == pc3 && pc == pc3
         end
 
         @testset "hash" begin
             @test hash(pc) == hash(pc)
-            pc2 = PartitionalClustering([1 1; 0 1], [-1 1; 1 0],
-                    [0 1.0; 1.0 0], [1.0 0.0; 0.3 0.7], [0.5 1])
+            pc2 = PartitionalClustering([0 1; 1 0; 1 1], [0 0 0; 0 0 0; 0 0 0],
+                    [0 0 0; 0 0 0; 0 0 0], [1.0 0.0; 0.0 1.0; 0.5 0.5],
+                    [0 1; 1 0])
             @test pc == pc2 && hash(pc) == hash(pc2)
         end
 
         @testset "accessors" begin
-            @test data(pc) == [1 1; 0 1]
-            @test constraints(pc) == [-1 1; 1 0]
-            @test weights(pc) == [0 1.0; 1.0 0]
-            @test assignments(pc) == [1.0 0.0; 0.3 0.7]
-            @test centers(pc) == [0.5 1]
-            @test θ(pc) == ([0.5 1])
+            @test data(pc) == [0 1; 1 0; 1 1]
+            @test constraints(pc) == [0 0 0; 0 0 0; 0 0 0]
+            @test weights(pc) == [0 0 0; 0 0 0; 0 0 0]
+            @test assignments(pc) == [1.0 0.0; 0.0 1.0; 0.5 0.5]
+            @test centers(pc) == [0 1; 1 0]
+            @test θ(pc) == ([0 1; 1 0])
         end
     end
 
     @testset "hierarchical clustering" begin
-        C = rand([-1, 0, 1], 2, 2, 2)
-        W = rand(2, 2, 2) .+ 1
-        hc = HierarchicalClustering([1 2; 0 2], C, W)
+        C = rand([-1, 0, 1], 3, 3, 3)
+        W = rand(3, 3, 3) .+ 1
+        hc = HierarchicalClustering([1 2; 0 2; 0 3], C, W)
 
         @testset "constructors" begin
-            @test hc.X == [1 2; 0 2]
+            @test hc.X == [1 2; 0 2; 0 3]
             @test hc.C == C
             @test hc.W == W
         end
 
         @testset "accessors" begin
-            @test data(hc) == [1 2; 0 2]
+            @test data(hc) == [1 2; 0 2; 0 3]
             @test constraints(hc) == C
             @test weights(hc) == W
         end
