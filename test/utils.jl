e8a7ee64d23274ee187e9fabeb756308a49a8341
laschuet <laschuet@users.noreply.github.com>
2019-06-14 14:56:47 +0200

    Extend test case

--- test/utils.jl
+++ test/utils.jl
@@ -7,10 +7,10 @@
     end
 
     @testset "matrix subtraction operator" begin
-        A = [1 1; 2 2]
-        B = [3 3 3; 4 4 4]
-        @test VAIML.sub(A, A) == [0 0; 0 0]
-        @test VAIML.sub(A, B) == [-2 -2 3; -2 -2 4]
-        @test VAIML.sub(B, A) == [2 2 3; 2 2 4]
+        A = [1 1 1; 2 2 2]
+        B = [1 1; 2 2; 3 3]
+        @test VAIML.sub(A, A) == [0 0 0; 0 0 0]
+        @test VAIML.sub(A, B) == [0 0 1; 0 0 2; 3 3 nothing]
+        @test VAIML.sub(B, A) == [0 0 1; 0 0 2; 3 3 nothing]
     end
 end
