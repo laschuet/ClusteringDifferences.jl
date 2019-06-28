5b0ed961720955ecb09045bed3fe81b649652a88
laschuet <laschuet@users.noreply.github.com>
2019-06-28 13:09:58 +0200

    Add tests for k-means

--- test/kmeans.jl
+++ test/kmeans.jl
@@ -1,4 +1,15 @@
 @testset "kmeans!" begin
-    clusterings = kmeans!([1 0; 0 1; -1 0; 0 -1], [1 1; -1 -1])
-    println(clusterings)
+    X = [1 0; 0 1; -1 0; 0 -1]
+    M = [1 1; -1 -1]
+    n, m = size(X)
+    k, = size(M)
+
+    clusterings = kmeans!(X, M)
+    c = clusterings[end]
+
+    @test isa(clusterings, Vector{PartitionalClustering})
+    @test c.X == X
+    @test size(c.Y) == (n, k)
+    @test all(y -> 0 <= y <= 1, c.Y)
+    @test size(c.M) == (k, m)
 end
