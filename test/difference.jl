b99ffb69ca89ef74375c04031ebe9ff0d8d276c1
laschuet <laschuet@users.noreply.github.com>
2019-06-29 17:43:34 +0200

    Test type

--- test/difference.jl
+++ test/difference.jl
@@ -31,15 +31,18 @@
 
     @testset "subtraction operator" begin
         cd = c - c
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == [0 0; 0 0; 0 0]' && cd.C == [0 0 0; 0 0 0; 0 0 0]'
                 && cd.W == [0 0 0; 0 0 0; 0 0 0]'
                 && cd.Y == [0.0 0.0; 0.0 0.0; 0.0 0.0]' && cd.M == [0 0; 0 0]'
                 && cd.k == 0 && cd.Y_MASK == [0 0; 0 0; 0 0]'
                 && cd.M_MASK == [0 0; 0 0]')
         cd = c - c2
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == X && cd.C == C && cd.W == W && cd.Y == Y && cd.M == M
                 && cd.k == k && cd.Y_MASK == Y_MASK && cd.M_MASK == M_MASK)
         cd = c2 - c
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == X2 && cd.C == C2 && cd.W == W2 && cd.Y == Y2
                 && cd.M == M2 && cd.k == k2 && cd.Y_MASK == Y_MASK2
                 && cd.M_MASK == M_MASK2)
@@ -47,6 +50,7 @@
 
     @testset "forward difference" begin
         cd = forward([c, c2], 1)
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == X2 && cd.C == C2 && cd.W == W2 && cd.Y == Y2
                 && cd.M == M2 && cd.k == k2 && cd.Y_MASK == Y_MASK2
                 && cd.M_MASK == M_MASK2)
@@ -55,11 +59,13 @@
 
     @testset "backward difference" begin
         cd = backward([c, c2], 1)
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == c.X && cd.C == c.C && cd.W == c.W && cd.Y == c.Y
                 && cd.M == c.M && cd.k == size(c.M, 2)
                 && cd.Y_MASK == ones(Int, size(c.Y))
                 && cd.M_MASK == ones(Int, size(c.M)))
         cd = backward([c, c2], 2)
+        @test isa(cd, PartitionalClusteringDifference)
         @test (cd.X == X2 && cd.C == C2 && cd.W == W2 && cd.Y == Y2
                 && cd.M == M2 && cd.k == k2 && cd.Y_MASK == Y_MASK2
                 && cd.M_MASK == M_MASK2)
