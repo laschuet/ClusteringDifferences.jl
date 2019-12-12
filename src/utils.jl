cfd72ff5c4c25346ad33363f6fb2f0cbb63d7fe3
laschuet <laschuet@users.noreply.github.com>
2019-12-12 13:43:13 +0100

    Overhaul utilities

--- src/utils.jl
+++ src/utils.jl
@@ -1,162 +1,170 @@
 """
-    MatrixDifference{T<:Real}
+    MatrixChange{T<:Real}
 
-Matrix difference.
+Matrix change.
 """
-struct MatrixDifference{T<:Real}
+struct MatrixChange{T<:Real}
     M::SparseMatrixCSC{T}
-    A::SparseMatrixCSC{T}
-    R::SparseMatrixCSC{T}
+    AI::SubArray{T}
+    AJ::SubArray{T}
+    RI::SubArray{T}
+    RJ::SubArray{T}
 end
 
 """
-    modified(a::MatrixDifference)
+    modified(a::MatrixChange)
 
 Access the modified elements.
 """
-modified(a::MatrixDifference) = a.M
+modified(a::MatrixChange) = a.M
 
 """
-    added(a::MatrixDifference)
+    added(a::MatrixChange)
 
-Access the added elements.
+Access the tuple containing the elements added in each dimension.
 """
-added(a::MatrixDifference) = a.A
-
-"""
-    removed(a::MatrixDifference)
-
-Access the removed elements.
-"""
-removed(a::MatrixDifference) = a.R
+added(a::MatrixChange) = a.AI, a.AJ
 
 """
-    setchange(a::AbstractVector, b::AbstractVector)
+    added(a::MatrixChange, dim::Integer)
 
-Compute the difference between the vectors `a` and `b`, and return a tuple
-containing the unique elements added and removed.
+Access the elements added in dimension `d`.
+"""
+function added(a::MatrixChange, dim::Integer)
+    1 <= dim <= 2 || throw(ArgumentError("dimension $dim out of range (1:2)")
+    added(a)[dim]
+end
+
+"""
+    removed(a::MatrixChange)
+
+Access the tuple containing the elements removed from each dimension.
+"""
+removed(a::MatrixChange) = a.RI, a.RJ
+
+"""
+    removed(a::MatrixChange, dim::Integer)
+
+Access the elements removed from dimension `d`.
+"""
+function removed(a::MatrixChange, dim::Integer) =
+    1 <= dim <= 2 || throw(ArgumentError("dimension $dim out of range (1:2)")
+    removed(a)[dim]
+end
+
+"""
+"""
+function replace(a::AbstractVector{T}, d::AbstractDict{T,T}) where {T<:Integer}
+    t = similar(a)
+    @inbounds for i = 1:length(a)
+        t[i] = get(d, a[i], a[i])
+    end
+    return t
+end
+
+"""
+"""
+function replace!(a::AbstractVector{T}, d::AbstractDict{T,T}) where {T<:Integer}
+    @inbounds for i = 1:length(a)
+        a[i] = get(d, a[i], a[i])
+    end
+    return a
+end
+
+"""
+    change(a::AbstractVector, b::AbstractVector)
+
+Compute the change from vector `a` to vector `b`, and return a tuple containing
+the unique elements that have been shared, added and removed.
 
 # Examples
 ```jldoctest
-julia> setchange([1, 2, 3, 3], [4, 2, 1])
+julia> change([1, 2, 3, 3], [4, 2, 1])
 ([4], [3])
 ```
 """
-setchange(a::AbstractVector, b::AbstractVector) = setdiff(b, a), setdiff(a, b)
+change(a::AbstractVector, b::AbstractVector) =
+    intersect(a, b), setdiff(b, a), setdiff(a, b)
 
 """
     change(A::AbstractMatrix, B::AbstractMatrix, ia::AbstractDict, ja::AbstractDict, ib::AbstractDict, jb::AbstractDict)
 
-Compute the difference between the matrices `A` and `B`.
+Compute the change from matrix `A` to matrix `B`, and return a tuple containing
+the elements that have been modified, added (per row and column), removed (per
+row and column).
 """
 function change(A::AbstractMatrix, B::AbstractMatrix, ia::AbstractDict,
-                ja::AbstractDict, ib::AbstractDict, jb::AbstractDict)
+            ja::AbstractDict, ib::AbstractDict, jb::AbstractDict)
+    T = promote_type(eltype(A), eltype(B))
     iakeys = collect(keys(ia))
     jakeys = collect(keys(ja))
     ibkeys = collect(keys(ib))
     jbkeys = collect(keys(jb))
-    T = promote_type(eltype(A), eltype(B))
 
     # Compute modified values
-    modified = sparse([], [], T[])
-    imod = intersect(iakeys, ibkeys)
-    jmod = intersect(jakeys, jbkeys)
-    println("imod $imod")
-    println("jmod $jmod")
-    if length(imod) > 0 && length(jmod) > 0
-        ia2 = similar(imod)
-        ja2 = similar(jmod)
-        ib2 = similar(imod)
-        jb2 = similar(jmod)
-        @inbounds for k = 1:length(imod)
-            ia2[k] = ia[imod[k]]
-            ib2[k] = ib[imod[k]]
-        end
-        @inbounds for k = 1:length(jmod)
-            ja2[k] = ja[jmod[k]]
-            jb2[k] = jb[jmod[k]]
-        end
-        println("ia2 $ia2")
-        println("ja2 $ja2")
-        println("ib2 $ib2")
-        println("jb2 $jb2")
-        # TODO check if indsa, indsb are correct, or if there is another way
-        # to use it in the views
-        indsa = CartesianIndex.(Iterators.product(sort!(ia2), sort!(ja2)))
-        indsb = CartesianIndex.(Iterators.product(sort!(ib2), sort!(jb2)))
-        println("indsa $indsa")
-        println("indsb $indsb")
-        modified = sparse(view(A, indsa) - view(B, indsb))
+    modval = sparse([], [], T[])
+    i = intersect(iakeys, ibkeys)
+    j = intersect(jakeys, jbkeys)
+    if length(i) > 0 && length(j) > 0
+        ia2 = replace(i, ia)
+        ja2 = replace(j, ja)
+        ib2 = replace(i, ib)
+        jb2 = replace(j, jb)
+        modval = sparse(view(A, ia2, ja2) - view(B, ib2, jb2))
     end
 
     # Compute added values
-    added = sparse([], [], T[])
-    iadd = setdiff(ibkeys, iakeys)
-    jadd = setdiff(jbkeys, jakeys)
-    if length(iadd) > 0 && length(jadd) <= 0
-        ib2 = similar(iadd)
-        @inbounds for k = 1:length(iadd)
-            ib2 = ib[iadd[k]]
-        end
-        added = sparse(view(B, ib2, :))
+    addival = T[]
+    addjval = T[]
+    i = setdiff(ibkeys, iakeys)
+    j = setdiff(jbkeys, jakeys)
+    if length(i) > 0 && length(j) <= 0
+        # Only rows have been added
+        replace!(i, ib)
+        addival = view(B, i, :)
     end
-    if length(iadd) <= 0 && length(jadd) > 0
-        jb2 = similar(jadd)
-        @inbounds for k = 1:length(jadd)
-            jb2 = jb[jadd[k]]
-        end
-        added = sparse(view(B, :, jb2))
+    if length(i) <= 0 && length(j) > 0
+        # Only columns have been added
+        replace!(j, jb)
+        addjval = view(B, :, j)
     end
-    if length(iadd) > 0 && length(jadd) > 0
-        ib2 = similar(iadd)
-        @inbounds for k = 1:length(iadd)
-            ib2 = ib[iadd[k]]
-        end
-        jb2 = similar(jadd)
-        @inbounds for k = 1:length(jadd)
-            jb2 = jb[jadd[k]]
-        end
-        indsi = CartesianIndex.(Iterators.product(ib2, 1:size(B, 2)))
-        indsj = CartesianIndex.(Iterators.product(1:size(B, 1), jb2))
-        append!(indsi, indsj)
-        unique!(indsi)
-        modified = sparse(view(B, indsi))
+    if length(i) > 0 && length(j) > 0
+        # Rows and columns have been added
+        replace!(i, ib)
+        replace!(j, jb)
+        addival = view(B, i, :)
+        addjval = view(B, :, j)
     end
 
     # Compute removed values
-    removed = sparse([], [], T[])
-    irem = setdiff(iakeys, ibkeys)
-    jrem = setdiff(jakeys, jbkeys)
-    if length(irem) > 0 && length(jrem) <= 0
-        ia2 = similar(irem)
-        @inbounds for k = 1:length(irem)
-            ia2 = ia[irem[k]]
-        end
-        added = sparse(view(A, ia2, :))
+    remival = T[]
+    remjval = T[]
+    i = setdiff(iakeys, ibkeys)
+    j = setdiff(jakeys, jbkeys)
+    if length(i) > 0 && length(j) <= 0
+        # Only rows have been removed
+        replace!(i, ia)
+        remival = view(A, i, :)
     end
-    if length(irem) <= 0 && length(jrem) > 0
-        ja2 = similar(jrem)
-        @inbounds for k = 1:length(jadd)
-            ja2 = ja[jrem[k]]
-        end
-        added = sparse(view(A, :, ja2))
+    if length(i) <= 0 && length(j) > 0
+        # Only columns have been removed
+        replace!(j, ja)
+        remjval = view(A, :, j)
     end
-    if length(irem) > 0 && length(jrem) > 0
-        ia2 = similar(irem)
-        @inbounds for k = 1:length(irem)
-            ia2 = ia[irem[k]]
-        end
-        ja2 = similar(jrem)
-        @inbounds for k = 1:length(jrem)
-            ja2 = ja[jrem[k]]
-        end
-        indsi = CartesianIndex.(Iterators.product(ia2, 1:size(A, 2)))
-        indsj = CartesianIndex.(Iterators.product(1:size(A, 1), ja2))
-        append!(indsi, indsj)
-        unique!(indsi)
-        modified = sparse(view(A, indsi))
+    if length(i) > 0 && length(j) > 0
+        # Rows and columns have been removed
+        replace!(i, ia)
+        replace!(j, ja)
+        remival = view(A, i, :)
+        remjval = view(A, :, j)
     end
 
-    return modified, added, removed
+    return modval, addival, addjval, remival, remjval
 end
-#diff(A::AbstractMatrix, B::AbstractMatrix) = diff(A, B, [], [], [], [])
+function change(A::AbstractMatrix, B::AbstractMatrix)
+    ia = Dict(i => i for i = 1:size(A, 1)
+    ja = Dict(i => i for i = 1:size(A, 2)
+    ib = Dict(i => i for i = 1:size(B, 1)
+    jb = Dict(i => i for i = 1:size(B, 2)
+    return change(A, B, ia, ja, ib, jb)
+end
