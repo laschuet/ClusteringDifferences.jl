53f883e892e307a9e224066dc435661b92968e1e
laschuet <laschuet@users.noreply.github.com>
2021-02-13 09:30:36 +0100

    Update dependencies

--- test/difference.jl
+++ test/difference.jl
@@ -3,24 +3,23 @@
             [0 0 0; 0 0 0; 0 0 0], [1 0 0.5; 0 1 0.5], (μ=[0 1; 1 0],))
     pc2 = PartitionalClustering([1, 2], [1, 2, 3], [0 0 -1; 0 0 -1; -1 -1 0],
             [0 0 1; 0 0 1; 1 1 0], [1 0 0; 0 1 0; 0 0 1], (μ=[0 1 1; 1 0 1],))
-    E = Vector(undef, 0)
     # pc - pc2
-    r = SetDifference([1, 2], Int[], Int[])
-    c = SetDifference([1, 2, 3], Int[], Int[])
-    C = MatrixDifference(sparse([0 0 1; 0 0 1; 1 1 0]), E, E)
-    W = MatrixDifference(sparse([0 0 -1; 0 0 -1; -1 -1 0]), E, E)
-    Y = MatrixDifference(sparse([0 0 0.5; 0 0 0.5]), [0, 0, 1], E)
-    p = NamedTupleDifference((μ=MatrixDifference(sparse([0 0; 0 0]), [1, 1], E),), NamedTuple(), NamedTuple())
+    r = SetDifference(Set([1, 2]), Set(), Set())
+    c = SetDifference(Set([1, 2, 3]), Set(), Set())
+    C = MatrixDifference(sparse([0, 0, 1, 0, 0, 1, 1, 1, 0]), [], [])
+    W = MatrixDifference(sparse([0, 0, -1, 0, 0, -1, -1, -1, 0]), [], [])
+    Y = MatrixDifference(sparse([0, 0, 0, 0, 0.5, 0.5]), [0, 0, 1], [])
+    p = NamedTupleDifference((μ=MatrixDifference(sparse([0, 0, 0, 0]), [1, 1], []),), NamedTuple(), NamedTuple())
     cd = PartitionalClusteringDifference(r, c, C, W, Y, p)
     cd2 = PartitionalClusteringDifference(r, c, C, W, Y, p)
     cd3 = PartitionalClusteringDifference(r, c, C, W, Y, p)
     # pc2 - pc
-    r2 = SetDifference([1, 2], Int[], Int[])
-    c2 = SetDifference([1, 2, 3], Int[], Int[])
-    C2 = MatrixDifference(sparse([0 0 -1; 0 0 -1; -1 -1 0]), E, E)
-    W2 = MatrixDifference(sparse([0 0 1; 0 0 1; 1 1 0]), E, E)
-    Y2 = MatrixDifference(sparse([0 0 -0.5; 0 0 -0.5]), E, [0, 0, 1])
-    p2 = NamedTupleDifference((μ=MatrixDifference(sparse([0 0; 0 0]), E, [1, 1]),), NamedTuple(), NamedTuple())
+    r2 = SetDifference(Set([1, 2]), Set(), Set())
+    c2 = SetDifference(Set([1, 2, 3]), Set(), Set())
+    C2 = MatrixDifference(sparse([0, 0, -1, 0, 0, -1, -1, -1, 0]), [], [])
+    W2 = MatrixDifference(sparse([0, 0, 1, 0, 0, 1, 1, 1, 0]), [], [])
+    Y2 = MatrixDifference(sparse([0, 0, 0, 0, -0.5, -0.5]), [], [0, 0, 1])
+    p2 = NamedTupleDifference((μ=MatrixDifference(sparse([0, 0, 0, 0]), [], [1, 1]),), NamedTuple(), NamedTuple())
 
     @testset "constructors" begin
         @test isa(cd, PartitionalClusteringDifference)
@@ -45,18 +44,21 @@
         @test constraints(cd) == C
         @test weights(cd) == W
         @test assignments(cd) == Y
-        @test parameters(cd) == θ(cd) == p
+        @test parameters(cd) == p
     end
 
     @testset "subtraction operator" begin
+        pc = PartitionalClustering([1, 2], [1, 2, 3], [0 0 0; 0 0 0; 0 0 0],
+            [0 0 0; 0 0 0; 0 0 0], [1 0 0.5; 0 1 0.5], (μ=[0 1; 1 0],))
+
         cd = pc - pc
         @test isa(cd, PartitionalClusteringDifference)
-        @test (cd.r == SetDifference([1, 2], Int[], Int[])
-                && cd.c == SetDifference([1, 2, 3], Int[], Int[])
-                && cd.C == MatrixDifference(sparse([0 0 0; 0 0 0; 0 0 0]), E, E)
-                && cd.W == MatrixDifference(sparse([0 0 0; 0 0 0; 0 0 0]), E, E)
-                && cd.Y == MatrixDifference(sparse([0 0 0; 0 0 0]), E, E)
-                && cd.p == NamedTupleDifference((μ=MatrixDifference(sparse([0 0; 0 0]), E, E),), NamedTuple(), NamedTuple()))
+        @test (cd.r == SetDifference(Set([1, 2]), Set(), Set())
+                && cd.c == SetDifference(Set([1, 2, 3]), Set(), Set())
+                && cd.C == MatrixDifference(sparse([0, 0, 0, 0, 0, 0, 0, 0, 0]), [], [])
+                && cd.W == MatrixDifference(sparse([0, 0, 0, 0, 0, 0, 0, 0, 0]), [], [])
+                && cd.Y == MatrixDifference(sparse([0, 0, 0, 0, 0, 0]), [], [])
+                && cd.p == NamedTupleDifference((μ=MatrixDifference(sparse([0, 0, 0, 0]), [], []),), NamedTuple(), NamedTuple()))
         cd = pc - pc2
         @test isa(cd, PartitionalClusteringDifference)
         @test (cd.r == r && cd.c == c && cd.C == C && cd.W == W && cd.Y == Y
